58c97e46c85ba82c65dfaa2b95a98c90543abecb
Jassayy <jasgandhi2004@gmail.com>
2024-09-21 20:22:57 +0530

    timepass patterns

---
+++ patterns.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+      int n;
+     cout << "say a number: " << endl;
+     cin >> n;
+
+      // for(int i = 0 ; i<n ; i++){
+      //       for(int j = 0 ; j<n ; j++){
+      //             cout<<"*";
+      //       }
+      //       cout<<endl;
+      // }
+
+       for(int i = 0 ; i<=n ; i++){
+            for(int j = 0 ; j<i ; j++){
+                  cout<<"*";
+            }
+            cout<<endl;
+      }
+
+      return 0;
+}
