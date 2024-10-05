fb19ab912da55bd2a6801f837aeb67bd77ab3e8e
Jassayy <jasgandhi2004@gmail.com>
2024-10-05 13:33:19 +0530

    pass by reference in pointer

--- pointers.cpp
+++ pointers.cpp
@@ -2,12 +2,21 @@
 
 using namespace std;
 
+void changeA(int *ptr)
+{
+      *ptr = 20;
+}
 int main()
 {
-      // int *ptr;
-      // or
-      int *ptr = NULL;
-      cout << "Null pointer: " << ptr; // garbage value
-      // we cant dereference the null pointer
+      // pass by reference
+      int a = 10;
+
+      cout << "a: " << a << endl;
+
+      // changeA(a);  this will not reflect in main function cuz it is pass by value..so use &a to pass mem address of int a
+
+      changeA(&a);
+      cout << "After pass by ref: a : " << a << endl;
+
       return 0;
 }
