3012abd904649c2e47f105c7a655f8d0e650bfb7
Jassayy <jasgandhi2004@gmail.com>
2024-10-05 12:53:01 +0530

    double dereferencing

--- pointers.cpp
+++ pointers.cpp
@@ -5,27 +5,9 @@ using namespace std;
 int main()
 {
       int a = 10;
-      cout << &a << endl;
-      // output 0x61ff0c  this is the memory address of int 'a' and & operator is used to print this value
+      int* ptr = &a;
+      int** dptr = &ptr;
 
-      int *ptr = &a;
-      cout << ptr << endl;
-      // output 0x61ff0c  this is the same memory address printed by & operator as above
-
-      cout << &ptr << endl;
-      // also print address of pointer 0x61ff04
-
-      // can do same with other data types...just the data type of pointer should be same as variable
-
-      int **ptr2 = &ptr;
-      cout << ptr2 << endl;
-      // output 0x61ff04  this is the memory address of pointer ptr and & operator is used to print this value
-
-      // we can also dereference the pointer using * operator to access the value at that memory address
-      int b = 60;
-      int* pointer = &b;
-
-      cout << "value at mem location " << pointer << " is " << *pointer << endl;
-      
+      cout << "double derefernce: " << **(dptr) << endl; //output should be 10
       return 0;
 }
