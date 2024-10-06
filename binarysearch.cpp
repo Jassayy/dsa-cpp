126125eb9201e661caec03e99d44c723098f3914
Jassayy <jasgandhi2004@gmail.com>
2024-10-06 12:57:44 +0530

    recursive binary search

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -2,9 +2,8 @@
 #include <vector>
 using namespace std;
 
-int binarySearch(vector<int> arr, int target)
+int binarySearch(vector<int> arr, int target, int st, int end)
 {
-      int st = 0, end = arr.size() - 1;
 
       while (st <= end)
       {
@@ -12,11 +11,11 @@ int binarySearch(vector<int> arr, int target)
 
             if (target > arr[mid])
             {
-                  st = mid + 1;
+                  return binarySearch(arr, target, mid + 1, end);
             }
             else if (target < arr[mid])
             {
-                  end = mid - 1;
+                  return binarySearch(arr, target, st, mid - 1);
             }
             else
             {
@@ -30,11 +29,11 @@ int main()
       vector<int> arr = {-1, 0, 3, 4, 5, 9, 12}; // odd vector
       int target = 12;
 
-      cout << "element found at index: " << binarySearch(arr, target) << endl;
+      cout << "element found at index: " << binarySearch(arr, target, 0, arr.size() - 1) << endl;
 
       vector<int> arr2 = {0, 3, 5, 7, 8, 9}; // even vector
       int target2 = 7;
 
-      cout << "element found at index: " << binarySearch(arr2, target2) << endl;
+      cout << "element found at index: " << binarySearch(arr2, target2, 0, arr2.size() - 1) << endl;
       return 0;
 }
