b7c9a7dfbf7a3105b8424c6463ef33febc59fd0d
Jassayy <jasgandhi2004@gmail.com>
2024-10-19 23:40:09 +0530

    stringa and character arrays

---
+++ strings.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstring>
+#include<string>
+using namespace std;
+
+int main()
+{
+      //CHARACTER ARRAYS
+      // char str[] = "helloo world";
+      // cout << str << endl;
+      // cout << strlen(str) << endl;
+      // cout << str[3] << endl;
+
+      // char str2[1000];
+
+      // cin.getline(str2, 100, '$'); // use cin.getline for taking string inputs and delimiter refers to when the input should stop coming...here '$' is there...that means unless and until u write dollar sgn ...evrything is gona be the input
+
+      // cout << str2 << endl;
+
+      // char str[] = "jassi paaji";
+
+      // for (char c : str)
+      // {
+      //       cout << c << " ";
+      // }
+      // cout << endl;
+
+      //STRINGS
+
+      //strings are dynamic in nature but character arrays are not dynamic...
+
+      // string str = "hello jassi paaji";
+      // string str1 = "wsg";
+
+      // string str2 = str + str1;
+
+      // cout << str2 << endl;
+      return 0;
+}
