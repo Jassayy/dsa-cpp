ef2c89fa7f1c1d98c76782ac6973bffa3c75b2e8
Jassayy <jasgandhi2004@gmail.com>
2025-07-06 18:20:31 +0530

    optimization in rat in maze

--- backtracking.cpp
+++ backtracking.cpp
@@ -40,10 +40,10 @@ using namespace std;
 
 // RATS IN A MAZE PROBLEM
 
-void helper(vector<vector<int>> &mat, int row, int col, string path, vector<string> &ans, vector<vector<bool>> &vis)
+void helper(vector<vector<int>> &mat, int row, int col, string path, vector<string> &ans)
 {
           // BASE CASE
-          if (row < 0 || col < 0 || row >= mat.size() || col >= mat.size() || mat[row][col] == 0 || vis[row][col] == true)
+          if (row < 0 || col < 0 || row >= mat.size() || col >= mat.size() || mat[row][col] == 0 || mat[row][col] == -1)
           {
                     return;
           }
@@ -54,28 +54,27 @@ void helper(vector<vector<int>> &mat, int row, int col, string path, vector<stri
                     return;
           }
 
-          vis[row][col] = true;
+          mat[row][col] = -1;
 
           // down
-          helper(mat, row + 1, col, path + 'D', ans, vis);
-          // up 
-          helper(mat, row - 1, col, path + 'U', ans, vis);
-          // left 
-          helper(mat, row, col - 1, path + 'L', ans, vis);
-          // right 
-          helper(mat, row, col + 1, path + 'R', ans, vis);
+          helper(mat, row + 1, col, path + 'D', ans);
+          // up
+          helper(mat, row - 1, col, path + 'U', ans);
+          // left
+          helper(mat, row, col - 1, path + 'L', ans);
+          // right
+          helper(mat, row, col + 1, path + 'R', ans);\
 
-          vis[row][col] = false;
+          mat[row][col] = 1;
 }
 
 vector<string> findPath(vector<vector<int>> &mat)
 {
           int n = mat.size();
-          vector<vector<bool>> vis(n, vector<bool>(n, false));
 
           vector<string> ans;
           string path = "";
-          helper(mat, 0, 0, path, ans, vis);
+          helper(mat, 0, 0, path, ans);
 
           return ans;
 }
