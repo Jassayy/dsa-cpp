8aaafc9c26fc79101c6094d105e06dfc8d2b5915
Jassayy <jasgandhi2004@gmail.com>
2025-08-03 22:31:39 +0530

    dp

---
+++ dp.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    
+
+    return 0;
+}
+
+//memoization            |  top down
+//tabulation             |  bottom up (go from base case to required case)
+//space optimisation     | approach
+//
